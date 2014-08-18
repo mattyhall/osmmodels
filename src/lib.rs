4129e839be7700cfcb90f9d3884494e2422d7730
Matthew Hall <matthew@quickbeam.me.uk>
2014-08-18 11:49:34 +0100

    Can create obj files. Model is not big enough
    Currently we use the latitude/longitude as the coordinates of the
    vertices in the obj file. This is too small

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::io::Writer;
 
 #[deriving(Show)]
 pub enum WavefrontValue {
-    Vertex(f32, f32, f32),
+    Vertex(f64, f64, f64),
     Face(Vec<int>)
 }
 
@@ -18,6 +18,7 @@ impl WavefrontValue {
     }
 }
 
+#[deriving(Show)]
 pub struct Wavefront {
     pub values: Vec<WavefrontValue>
 }
@@ -27,12 +28,12 @@ impl Wavefront {
         Wavefront {values: Vec::new()}
     }
 
-    pub fn to_string(&self) {
+    pub fn to_string(&self) -> String {
         let vals: Vec<String> = self.values.iter().map(|v| v.to_string()).collect();
-        vals.connect("\n");
+        vals.connect("\n")
     }
 
-    pub fn add_vertex(&mut self, x: f32, y: f32, z: f32) {
+    pub fn add_vertex(&mut self, x: f64, y: f64, z: f64) {
         self.values.push(Vertex(x, y, z));
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate track_visulisation;
 extern crate osmxml;
+extern crate cgmath;
 
 use std::collections::HashMap;
 use osmxml::{Osm, OsmElement, Relation, Way, Node};
-use track_visulisation::{Vertex, Face};
+use track_visulisation::{Wavefront};
+use cgmath::{Vector2, EuclideanVector, Vector};
 
 fn expand_element(elem: &OsmElement, elements: &HashMap<int, OsmElement>) -> Vec<(f64, f64)> {
     let refs = match *elem {
@@ -22,6 +24,42 @@ fn expand_element(elem: &OsmElement, elements: &HashMap<int, OsmElement>) -> Vec
     latlngs
 }
 
+type Vec2 = Vector2<f64>;
+
+fn top(w: &mut Wavefront, height: f64, a: Vec2, a1: Vec2, b: Vec2, b1: Vec2) {
+    w.add_vertex(a.x, a.y, height);
+    w.add_vertex(a1.x, a1.y, height);
+    w.add_vertex(b1.x, b1.y, height);
+    w.add_vertex(b.x, b.y, height);
+    w.add_face(vec!(-1, -2, -3, -4));
+}
+
+fn side(w: &mut Wavefront, height: f64, a: Vec2, b: Vec2) {
+    w.add_vertex(a.x, a.y, height);
+    w.add_vertex(b.x, b.y, height);
+    w.add_vertex(b.x, b.y, 0.0);
+    w.add_vertex(a.x, a.y, 0.0);
+    w.add_face(vec!(-1, -2, -3, -4));
+}
+
+fn to_wavefront(thickness: f64, height: f64, latlngs: Vec<(f64, f64)>) -> Wavefront {
+    let mut iter = latlngs.iter().zip(latlngs.iter().skip(1));
+    let mut w = Wavefront::new();
+    for (&(ax, ay), &(bx, by)) in iter {
+        let a = Vector2::new(ax, ay);
+        let b = Vector2::new(bx, by);
+        let ab = Vector2::new(bx - ax, by - ay).normalize();
+        let p = Vector2::new(-ab.y, ab.x);
+        let a1 = a + p.mul_s(thickness);
+        let b1 = b + p.mul_s(thickness);
+        top(&mut w, height, a, a1, b, b1);
+        top(&mut w, 0.0, a, a1, b, b1);
+        side(&mut w, height, a, b);
+        side(&mut w, height, a1, b1);
+    }
+    w
+}
+
 fn main() {
     let path = &Path::new("spa.osm");
     let osm = Osm::new(path).unwrap();
@@ -34,7 +72,6 @@ fn main() {
             _ => false
         }
     }).next().unwrap();
-    println!("{}", expand_element(relation, &osm.elements));
-    println!("{}", Vertex(10.0, 10.0, 10.0).to_string());
-    println!("{}", Face(vec!(-1, -2, -3, -4)).to_string());
+    let obj = to_wavefront(0.001, 0.005, expand_element(relation, &osm.elements));
+    println!("{}", obj.to_string());
 }
